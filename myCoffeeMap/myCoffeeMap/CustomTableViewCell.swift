add7470003b0071bb66170397143fcd4be264b25
maxim-sulim <cool.sulim@list.ru>
2023-01-27 20:04:13 +0500

    castom table view and restaroant image

---
+++ myCoffeeMap/myCoffeeMap/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  myCoffeeMap
+//
+//  Created by Максим Сулим on 27.01.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var lacationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+}
--- myCoffeeMap/myCoffeeMap/MainViewController.swift
+++ myCoffeeMap/myCoffeeMap/MainViewController.swift
@@ -26,11 +26,12 @@ class MainViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = restaurantName[indexPath.row]
-        cell.imageView?.image = UIImage(named: restaurantName[indexPath.row])
-        cell.imageView?.layer.cornerRadius = 85 / 2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        
+        cell.nameLable.text = restaurantName[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: restaurantName[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds = true
         return cell
     }
     
